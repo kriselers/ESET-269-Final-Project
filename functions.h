588b957246da306d6ecc37335345a7d8c5e691ec
alexliu21 <58192497+alexliu21@users.noreply.github.com>
2019-12-04 10:58:35 -0600

    Add files via upload

---
+++ functions.h
@@ -0,0 +1,35 @@
+//
+//  functions.h
+//  ESET269Final
+//
+//  Created by Kris Elers on 11/18/19.
+//  Copyright © 2019 Kris Elers. All rights reserved.
+//
+
+#ifndef functions_h
+#define functions_h
+
+#include <stdio.h>
+
+//Structs
+
+//Functions
+void UARTinit(void);
+void ADCinit(void);
+void PINinitDigital(void);
+void PINinitRGB(void);
+
+void getMenu(void);
+void Uprint(char string[]);
+void mainMenu(void);
+
+void rgbControl(void);
+void digitalInput(void);
+void tempReading(void);
+
+int ADCRead(void);
+int getChoice(void);
+
+//Constants
+
+#endif /* functions_h */
